df78a24793f9ceebeaf1820cbd11a98f1c376a49
Andrea Cecchi <andrea.cecchi85@gmail.com>
2015-09-25 10:28:48 +0200

    some changes

--- src/collective/editablemenu/browser/menu_support_view.py
+++ src/collective/editablemenu/browser/menu_support_view.py
@@ -9,12 +9,12 @@ from Products.CMFCore.interfaces import IFolderish
 class MenuSupportView(BrowserView):
     """
     """
+    registry = "collective.editablemenu.browser.interfaces.IEditableMenuSettings.menu_tabs"
 
     @property
     @view.memoize
     def menu_settings(self):
-        registry = "collective.editablemenu.browser.interfaces.IEditableMenuSettings.menu_tabs"
-        return api.portal.get_registry_record(registry)
+        return api.portal.get_registry_record(self.registry)
 
     def get_menu_tabs(self):
         context = self.context.aq_inner
@@ -27,25 +27,29 @@ class MenuSupportView(BrowserView):
             tab_title = getattr(tab_settings, "tab_title", '')
             if not tab_title:
                 continue
+            tab_dict = {'index': i}
             # this text is used inside a link, so i can't use portal_transorms
             # because it wraps all inside a <p> tag.
             # I wrap every row inside a span, so they can be easily styled
             rows = ["<span>%s</span>" % x for x in tab_title.split("\r\n")]
-            formatted_tab_title = "<br/>".join(rows)
+            tab_dict['title'] = "<br/>".join(rows)
             navigation_folder = self.get_navigation_folder(tab_settings)
-            if not navigation_folder:
-                continue
-            navigation_folder_path = "/".join(navigation_folder.getPhysicalPath())
-            results.append(
-                {'index': i,
-                'title': formatted_tab_title,
-                'url': navigation_folder.absolute_url(),
-                'selected': context_path.startswith(navigation_folder_path)}
-            )
+            if navigation_folder:
+                tab_dict['url'] = navigation_folder.absolute_url()
+                tab_dict['selected'] = "/".join(navigation_folder.getPhysicalPath())
+            results.append(tab_dict)
+            # results.append(
+            #     {'index': i,
+            #     'title': formatted_tab_title,
+            #     'url': navigation_folder.absolute_url(),
+            #     'selected': context_path.startswith(navigation_folder_path)}
+            # )
         return results
 
     def get_navigation_folder(self, tab_settings):
         folder_uid = getattr(tab_settings, "navigation_folder", "")
+        if not folder_uid:
+            return None
         return api.content.get(UID=folder_uid)
 
     def get_additional_columns(self, tab_settings):
@@ -61,38 +65,39 @@ class MenuSupportView(BrowserView):
 class SubMenuDetailView(MenuSupportView):
     """
     """
-    def get_menu_subitems(self):
-        return {'dynamic_items': self.get_dynamic_items(),
-                'static_items': self.get_static_items}
+    def get_menu_subitems(self, tab_id=None):
+        if tab_id is None:
+            tab_id = self.request.form.get('tab_id')
+        if tab_id is None:
+            return {}
+        settings = self.get_selected_tab(tab_id)
+        if not settings:
+            return {}
+        return {'dynamic_items': self.get_dynamic_items(settings),
+                'static_items': self.get_static_items(settings)}
 
-    @property
-    @view.memoize
-    def selected_tab(self):
-        if 'tab_id' not in self.request.form:
-            return None
-        try:
-            selected_id = int(self.request.form.get('tab_id'))
-        except ValueError:
-            logger.error(
-                "Invalid index number (%s). Unable to retrieve configuration." % self.request.form.get('tab_id')
-            )
-            return None
+    def get_selected_tab(self, tab_id):
+        if isinstance(tab_id, str):
+            try:
+                tab_id = int(tab_id)
+            except ValueError:
+                logger.error(
+                    "Invalid index number (%s). Unable to retrieve configuration." % tab_id
+                )
+                return None
         settings = self.menu_settings
         if not settings:
             return None
         try:
-            return settings[selected_id]
+            return settings[tab_id]
         except IndexError:
             logger.error(
-                "Index(%s) not found in menu settings. Unable to retrieve configuration." % selected_id
+                "Index(%s) not found in menu settings. Unable to retrieve configuration." % tab_id
             )
             return None
         return None
 
-    def get_dynamic_items(self):
-        settings = self.selected_tab
-        if not settings:
-            return []
+    def get_dynamic_items(self, settings):
         navigation_folder = self.get_navigation_folder(settings)
         if not navigation_folder:
             return []
@@ -111,10 +116,7 @@ class SubMenuDetailView(MenuSupportView):
                 results.append(result_dict)
         return results
 
-    def get_static_items(self):
-        settings = self.selected_tab
-        if not settings:
-            return []
+    def get_static_items(self, settings):
         additional_columns = self.get_additional_columns(settings)
         if not additional_columns:
             return []
