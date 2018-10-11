c3c0e452e9879bdb09560a664d7ee512a2a89618
nzambello <nicola.zambello@redturtle.it>
2018-10-11 09:56:28 +0200

    Preparing release 2.0.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='collective.editablemenu',
-    version='2.0.0.dev0',
+    version='2.0.0',
     description="A customizable dropdown menu",
     long_description=long_description,
     # Get more from http://pypi.python.org/pypi?%3Aaction=list_classifiers
