0e84f2139fcd378b351614baf51b7ebd18172ae7
Andrea Cecchi <andrea.cecchi85@gmail.com>
2019-06-28 17:57:11 +0200

    fix code-analysis

--- src/collective/editablemenu/testing.py
+++ src/collective/editablemenu/testing.py
@@ -42,44 +42,45 @@ class CollectiveEditablemenuRobotLayer(PloneSandboxLayer):
                     'simple_link': '',
                     'tab_title': 'Foo',
                     'additional_columns': '',
-                    'condition': 'python: True'},
+                    'condition': 'python: True',
+                },
                 {
                     'navigation_folder': '',
                     'simple_link': '',
                     'tab_title': 'Bar',
                     'additional_columns': '/folder',
-                    'condition': 'python: True'},
+                    'condition': 'python: True',
+                },
                 {
                     'navigation_folder': '',
                     'simple_link': '',
                     'tab_title': 'Baz',
                     'additional_columns': '',
-                    'condition': 'python: True'}
+                    'condition': 'python: True',
+                },
             ]
         }
         api.portal.set_registry_record(
             'menu_tabs_json',
             json.dumps(self.settings).decode('utf-8'),
-            interface=IEditableMenuSettings)
+            interface=IEditableMenuSettings,
+        )
         api.content.create(
-            container=self.portal,
-            type='Folder',
-            id='folder',
-            title='Folder'
+            container=self.portal, type='Folder', id='folder', title='Folder'
         )
         self.document1 = api.content.create(
             container=self.portal['folder'],
             type='Document',
             title='Document 1',
             id='document-1',
-            text='Text for document 1'
+            text='Text for document 1',
         )
         self.document2 = api.content.create(
             container=self.portal['folder'],
             type='Document',
             title='Document 2',
             id='document-2',
-            text='Text for document 2'
+            text='Text for document 2',
         )
 
 
@@ -88,13 +89,13 @@ COLLECTIVE_EDITABLEMENU_FIXTURE = CollectiveEditablemenuLayer()
 
 COLLECTIVE_EDITABLEMENU_INTEGRATION_TESTING = IntegrationTesting(
     bases=(COLLECTIVE_EDITABLEMENU_FIXTURE,),
-    name='CollectiveEditablemenuLayer:IntegrationTesting'
+    name='CollectiveEditablemenuLayer:IntegrationTesting',
 )
 
 
 COLLECTIVE_EDITABLEMENU_FUNCTIONAL_TESTING = FunctionalTesting(
     bases=(COLLECTIVE_EDITABLEMENU_FIXTURE,),
-    name='CollectiveEditablemenuLayer:FunctionalTesting'
+    name='CollectiveEditablemenuLayer:FunctionalTesting',
 )
 
 
@@ -102,7 +103,7 @@ COLLECTIVE_EDITABLEMENU_ACCEPTANCE_TESTING = FunctionalTesting(
     bases=(
         COLLECTIVE_EDITABLEMENU_FIXTURE,
         REMOTE_LIBRARY_BUNDLE_FIXTURE,
-        z2.ZSERVER_FIXTURE
+        z2.ZSERVER_FIXTURE,
     ),
-    name='CollectiveEditablemenuLayer:AcceptanceTesting'
+    name='CollectiveEditablemenuLayer:AcceptanceTesting',
 )
