24b5bdcbc6375fa1c99e1a223e954f3d4896d3ba
Andrea Cecchi <andrea.cecchi85@gmail.com>
2019-07-05 12:01:57 +0200

    Back to development: 2.2.1

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = '\n\n'.join(
 
 setup(
     name='collective.editablemenu',
-    version='2.2.0',
+    version='2.2.1.dev0',
     description="A customizable dropdown menu",
     long_description=long_description,
     # Get more from http://pypi.python.org/pypi?%3Aaction=list_classifiers
